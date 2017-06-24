20ed6b6cfddfd8314f8395a285ecfd6c2c102ffb
andry81 <andry@inbox.ru>
2017-06-24 11:01:34 +0000

    * scm scripts refactor
    git-svn-id: svn+ssh://svn.code.sf.net/p/svncmd/deploy/trunk@6 5f394a0f-7127-4653-9ccc-fe55d7a9570d

--- _svncmd/svncmd--Scripts_gh.sync_all.bat
+++ _svncmd/svncmd--Scripts_gh.sync_all.bat
@@ -2,11 +2,23 @@
 
 setlocal
 
+rem extract name of sunc directory from name of the script
+set "?~nx0=%~nx0"
+
+set "WCROOT_SUFFIX=%?~nx0:*.=%"
+
+set "WCROOT=%?~nx0%."
+if "%WCROOT_SUFFIX%" == "%?~nx0%" goto IGNORE_WCROOT_SUFFIX
+call set "WCROOT=%%WCROOT:.%WCROOT_SUFFIX%.=%%"
+
+:IGNORE_WCROOT_SUFFIX
+if "%WCROOT:~-1%" == "." set "WCROOT=%WCROOT:~0,-1%"
+
 if "%NEST_LVL%" == "" set NEST_LVL=0
 
 set /A NEST_LVL+=1
 
-pushd "%~dp0svncmd--Scripts" && (
+pushd "%~dp0%WCROOT%" && (
   call :CMD git pull origin master || ( popd & goto EXIT )
   call :CMD git svn fetch || ( popd & goto EXIT )
   call :CMD git svn rebase || ( popd & goto EXIT )
---
+++ _svncmd/svncmd_sf.update_all.bat
@@ -0,0 +1,35 @@
+@echo off
+
+setlocal
+
+rem extract name of sunc directory from name of the script
+set "?~nx0=%~nx0"
+
+set "WCROOT_SUFFIX=%?~nx0:*.=%"
+
+set "WCROOT=%?~nx0%."
+if "%WCROOT_SUFFIX%" == "%?~nx0%" goto IGNORE_WCROOT_SUFFIX
+call set "WCROOT=%%WCROOT:.%WCROOT_SUFFIX%.=%%"
+
+:IGNORE_WCROOT_SUFFIX
+if "%WCROOT:~-1%" == "." set "WCROOT=%WCROOT:~0,-1%"
+
+if "%NEST_LVL%" == "" set NEST_LVL=0
+
+set /A NEST_LVL+=1
+
+pushd "%~dp0%WCROOT%" && (
+  call :CMD svn up || ( popd & goto EXIT )
+)
+
+:EXIT
+set /A NEST_LVL-=1
+
+if %NEST_LVL% LEQ 0 pause
+
+exit /b
+
+:CMD
+echo.^>%*
+(%*)
+echo.
